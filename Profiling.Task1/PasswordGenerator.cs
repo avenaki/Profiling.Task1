06a2c131ff9ddb82ee722f5df210231e1e937f08
Viktoriia Gorneva <viktoriia.gorneva@ihsmarkit.com>
2022-07-01 22:35:33 +0300

    Add project files.

---
+++ Profiling.Task1/PasswordGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+
+namespace Profiling.Task1
+{
+    public static class PasswordGenerator
+    {
+
+        public static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+        {
+
+            var iterate = 10000;
+            var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Buffer.BlockCopy(salt, 0, hashBytes, 0, 16);
+            Buffer.BlockCopy(hash, 0, hashBytes, 16, 20);
+
+            var passwordHash = Convert.ToBase64String(hashBytes);
+
+            return passwordHash;
+
+        }
+
+    }
+}
---
+++ Profiling.Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Profiling.Task1;
+using System.Text;
+
+Console.WriteLine("");
+
+var salt = "ksbdhfrebasdhjkd";
+var saltBytes = Encoding.UTF8.GetBytes(salt);
+var passwordHash = PasswordGenerator.GeneratePasswordHashUsingSalt("superSecurePassword", saltBytes);
+
+Console.WriteLine(passwordHash);
